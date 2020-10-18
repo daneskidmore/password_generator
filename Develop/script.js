f8c781ff8c971600bb1654583f191f7abaca8650
Dane Skidmore <70667176+daneskidmore@users.noreply.github.com>
2020-10-17 17:36:50 -0700

    added password generator function

--- Develop/script.js
+++ Develop/script.js
@@ -17,11 +17,47 @@ var special;
 function getPasswordPreferences() {
   //a function to check that lenghs is between 8-12 and prompts to try again
   function lengthChecker(){
-    while (8 > passwordLength || 128< passwordLength) {
-      alert("Please give a length between 8-128")
-    }
-  }
+    while (8 > passwordLength || 128 < passwordLength) {
+      alert("Please give a length between 8-128");
+      passwordLength = prompg("Choose a length for your password from 8-128 characters.");
+  };
+};
+
+// a function to see if there was at least one character response then promtos to try again
+function checkforResponses() {
+  while(lowerCase === false && upperCase ==== false && numbers === fasle === special ==== false){
+    alert("please cheoose at least one tyoe of character.");
+    lowerCase = confirm("Would you like lower case letters?");
+    upperCase = confirm("Would you like upper case letters?");
+    numbers = confirm("Would you like numbers?");
+    special = confirm("Would you like special characters?");
+
+  };
+};
+
+//Prompts
+passwordLength = Number(prompt("Choose a length for your password from 8-128 characters"));
+lengthChecker();//calling the checking function
+lowerCase - confirm("Would you like lower case letters?");
+upperCase confirm("Would you like upper case letters?");
+numbers = confirm("Would you like numbeers");
+special = confirm("Would you like special characters?");
+checkforResponses ()/#$%/calling the checking function
+
+//function to get password
+function generatePassword(){
+    getPasswordPreferences();
+    
+  var lowerCaseCharacters = "abcdefghijklmnopqrstuvwxyz";
+  var upperCaseCharacters = "ABDEFGHIJKLMNOPQRSTUVWXYZ";
+  var specialCharacters = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  var numbersArray = ["0", "1", "2","3","4","5","6","7","8","9"];
 }
+
+
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
